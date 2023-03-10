4850a4a91578e5fae797991cb5d411c6d3c69e3b
Pratham Ashok Asrani <114286503+PrathamAsrani@users.noreply.github.com>
2023-03-10 23:29:29 +0530

    supporting files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js';
+
+let options = {
+    strings: ["Full Stack Web Development", "Cloud Computing: DevOps", "Data Management"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+};
+
+let typed = new Typed(".auto-type", options);
